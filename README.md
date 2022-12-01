cb372eb83c705d396ae98dc9418f8a9be4d326dd
Mikkel Blæsbjerg <mib@cacommodities.dk>
2022-12-01 22:46:12 +0100

    Added virtual environment and dependency installer

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # GeoGuessrAI
 Code for GeoGuessr project in Deep Learning
 
+Before running the file, install the packages by running install_dependencies.sh in bash.
+
 To run, open the notebook GeoGuessrAI.ipynb, and run all the cells. Explanations are in the notebook.
---
+++ install_dependencies.sh
@@ -0,0 +1,10 @@
+py -m venv venv
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  echo 'Activating virtual environment on linux' &&
+  source venv/bin/activate
+else
+  echo 'Activating virtual environment on windows' &&
+  source venv/Scripts/activate
+fi
+pip install -U pip
+pip install -r requirements.txt
